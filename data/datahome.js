20004969515e10192a8092e578a7b5c4af5a0fa5
Fabio Silveira <fabio.eds@hotmail.com>
2024-05-11 21:26:41 -0700

    AddedDataBaseMovies

--- data/datahome.js
+++ data/datahome.js
@@ -6,3 +6,47 @@ export const moviesHome = [
     "white chicks",
     "ted"
 ];
+
+const allMovies = [
+    {
+        genre: "Horror",
+        movies: ""
+    },
+    {
+        genre: "Thriller",
+        movies: ""
+    },
+    {
+        genre: "Marvel",
+        movies: ""
+    },
+    {
+        genre: "Drama",
+        movies: ""
+    },
+    {
+        genre: "Action",
+        movies: ""
+    },
+    {
+        genre: "Classics",
+        movies: ""
+    },
+    {
+        genre: "Comedy",
+        movies: ""
+    },
+    {
+        genre: "Science Fiction",
+        movies: [ "A.I. Artificial Intelligence", "Avatar" ,"Minority Report", "Star Trek II: The Wrath Of Khan", "Dune", "Planet Of The Apes", "Guardians Of The Galaxy", "Jurassic Park", "RoboCop", "The Terminator", "Arrival", "E.T. The Extra Terrestrial", "Aliens", "Back to The Future", "Terminator 2: Judgment Day", "Star Wars", "The Matrix", "Star Wars: The Empire Strikes Back", "Blade Runner", "The Fifth Element", "Inception" ]
+    },
+    {
+        genre: "Animes",
+        movies: [ "Dragon ball z", "Naruto shippuden", "Death note", "Yu Yu Hakusho", "Mob Psycho 100", "One Piece", "Fullmetal Alchemist: Brotherhood", "Pokemon", "Digimon", "Yu-Gi-Oh", "Saint Seiya: Knights of the Zodiac", "Samurai Champloo", "Hunter x Hunter", "Neon Genesis Evangelion", "FLCL", "Baccano", "Code Geass: Lelouch of the Rebellion", "Demon Slayer", "Ouran High School Host Club", "Made in Abyss", "My Hero Academia" ]
+    },
+    {
+        genre: "Series",
+        movies: [ "Breaking bad", "Better call saul", "Game of thrones", "House of dragon", "Pose", "Glamorous", "Vikings", "Euphoria", "White lotus", "Prison break", "Emily in paris", "The walking dead", "Queen Charlotte A Bridgerton Story","Supernatural", "Baby reindeer", "The Last Kingdom", "Beef", "Hightown", "The Simpsons", "Vikings Valhalla", "Blacklist" ]
+    },
+
+]
