62a247411c802e631b105c4fe8b39e252a5a2f3f
Fabio Silveira <fabio.eds@hotmail.com>
2024-05-11 22:31:49 -0700

    UpdatedDataBAse

--- data/datahome.js
+++ data/datahome.js
@@ -14,11 +14,11 @@ const allMovies = [
     },
     {
         genre: "Thriller",
-        movies: ""
+        movies: [ "War of the Worlds", "Blow Out", "Inside Man", "Get Out", "The invisible man", "The Dark Knight", "Parasite", "The Beekeeper", "The Handmaiden", "Zodiac", "Children of Men", "Jaws", "The black phone", "Alone", "The Guilty", "Fair play", "Missing", "Leave the world behind", "Deep water", "Sleeping Dogs", "Damaged" ]
     },
     {
         genre: "Marvel",
-        movies: ""
+        movies: [ "The Incredible Hulk", "Thor: The Dark World", "Doctor Strange", "Avengers: Age of Ultron", "Black Panther: Wakanda Forever", "Spider-Man: No Way Home", "Captain America: Civil War", "Iron Man", "The Avengers", "Spider-Man: Homecoming", "Thor: Ragnarok", "Captain America: The Winter Soldier", "Avengers: Endgame", "Captain America: The First Avenger", "Black Panther", "Shang-Chi and the Legend of the Ten Rings", "Captain Marvel", "Ant-Man", "Iron Man 3", "Iron Man 2", "Guardians of the Galaxy Vol. 3" ]
     },
     {
         genre: "Drama",
@@ -30,11 +30,11 @@ const allMovies = [
     },
     {
         genre: "Classics",
-        movies: ""
+        movies: [ "The godfather", "Home Alone", "Pretty Woman", "Dirty dance", "Ghost", "The blue lagoon", "Saturday Night Fever", "Face/Off", "Grease", "Titanic", "Forrest Gump", "Selena", "The Sixth Sense", "Men in Black", "Carrie", "Malcolm X", "There s Something About Mary", "Schindler's List", "Hoop Dreams", "The Fugitive", "A Nightmare on Elm Street" ]
     },
     {
         genre: "Comedy",
-        movies: ""
+        movies: ["Step Brothers", "White chicks", "ted", "Clueless", "Bridesmaids", "The hangover", "American pie", "School of rock", ""]
     },
     {
         genre: "Science Fiction",
