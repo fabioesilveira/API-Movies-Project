533ad28d842f4f3807ae3d10ad260ccda7c85836
Fabio Silveira <fabio.eds@hotmail.com>
2024-05-12 20:19:19 -0700

    UpdatedDataBase

--- data/datahome.js
+++ data/datahome.js
@@ -9,44 +9,54 @@ export const moviesHome = [
 
 const allMovies = [
     {
+        id: 1,
         genre: "Horror",
         movies: [ "Immaculate", "The Watchers", "Resident evil", "Abigail", "The jester", "talk to me", "The last thing mary saw", "Dead Silence", "Evil dead rise", "Smile", "US", "Night swim", "Sinister", "The visit", "saw", "A quiet place", "The black phone", "The ring", "When evil lurks", "Imaginary", "The exorcist:Believer" ]
     },
     {
+        id: 2,
         genre: "Thriller",
         movies: [ "War of the Worlds", "Blow Out", "Inside Man", "Get Out", "The invisible man", "The Dark Knight", "Parasite", "The Beekeeper", "The Handmaiden", "Zodiac", "Children of Men", "Jaws", "The Call", "Alone", "The Guilty", "Fair play", "Missing", "Leave the world behind", "Deep water", "Sleeping Dogs", "Damaged" ]
     },
     {
+        id: 3,
         genre: "Marvel",
         movies: [ "The Incredible Hulk", "Thor: The Dark World", "Doctor Strange", "Avengers: Age of Ultron", "Black Panther: Wakanda Forever", "Spider-Man: No Way Home", "Captain America: Civil War", "Iron Man", "The Avengers", "Spider-Man: Homecoming", "Thor: Ragnarok", "Captain America: The Winter Soldier", "Avengers: Endgame", "Captain America: The First Avenger", "Black Panther", "Shang-Chi and the Legend of the Ten Rings", "Captain Marvel", "Ant-Man", "Iron Man 3", "Iron Man 2", "Guardians of the Galaxy Vol. 3" ]
     },
     {
+        id: 4,
         genre: "Drama",
         movies: [ "Challengers", "Portrait of a Lady on Fire", "Boyz n the Hood", "Roma", "The green mile", "A beutiful mind", "unfaithful", "The Pursuit of Happyness", "Schindler s List", "Monster", "The Lover", "A star is born", "the greatest showman", "me before you", "My girl", "A Walk to Remember", "Call Me by Your Name", "The Lion King", "Marley & Me", "A Dog's Tale", "Memoirs of a Geisha"]
     },
     {
+        id: 5,
         genre: "Action",
         movies: ["Scarface", "Gladiator", "GoldenEye", "Die Another day", "Twister", "The mummy", "The day after tomorrow", "Colombiana", "The Woman King", "Batman Begins", "Crank", "The Mask of zorro", "Tomb Raider", "Damsel", "The iron claw", "Aquaman and the Lost Kingdom", "Fast & Furious", "Man on Fire", "I am Legend", "World War Z", "Charlie's Angels"]
     },
     {
+        id: 6,
         genre: "Classics",
         movies: [ "The godfather", "Home Alone", "Pretty Woman", "Dirty dance", "Ghost", "The blue lagoon", "Saturday Night Fever", "Face/Off", "Grease", "Titanic", "Forrest Gump", "Selena", "The Sixth Sense", "Men in Black", "Carrie", "Malcolm X", "There s Something About Mary", "Steel Magnolias", "Hoop Dreams", "The Fugitive", "A Nightmare on Elm Street" ]
     },
     {
+        id: 7,
         genre: "Comedy",
         movies: ["Step Brothers", "White chicks", "ted", "Clueless", "Bridesmaids", "The hangover", "American pie", "School of rock", "Unfrosted", "No Hard feelings", "We're the millers", "Joy ride", "Girls trip", "Crazy Rich Asians", "Palm Springs", "My cousin vinny", "Hot Pursuit", "Fading Gigolo", "How to Be Single", "The Hustle", "Little Man" ]
     },
     {
+        id: 8,
         genre: "Science Fiction",
         movies: [ "A.I. Artificial Intelligence", "Avatar" ,"Minority Report", "Star Trek II: The Wrath Of Khan", "Dune", "Planet Of The Apes", "Guardians Of The Galaxy", "Jurassic Park", "RoboCop", "The Terminator", "Arrival", "E.T. The Extra Terrestrial", "Aliens", "Back to The Future", "Terminator 2: Judgment Day", "Star Wars", "The Matrix", "Star Wars: The Empire Strikes Back", "Blade Runner", "The Fifth Element", "Inception" ]
     },
     {
+        id: 9,
         genre: "Animes",
         movies: [ "Dragon ball z", "Naruto shippuden", "Death note", "Yu Yu Hakusho", "Mob Psycho 100", "One Piece", "Fullmetal Alchemist: Brotherhood", "Pokemon", "Digimon", "Yu-Gi-Oh", "Saint Seiya: Knights of the Zodiac", "Samurai Champloo", "Hunter x Hunter", "Neon Genesis Evangelion", "FLCL", "Baccano", "Code Geass: Lelouch of the Rebellion", "Demon Slayer", "Ouran High School Host Club", "Made in Abyss", "My Hero Academia" ]
     },
     {
+        id: 10,
         genre: "Series",
         movies: [ "Breaking bad", "Better call saul", "Game of thrones", "House of dragon", "Pose", "Glamorous", "Vikings", "Euphoria", "White lotus", "Prison break", "Emily in paris", "The walking dead", "Queen Charlotte A Bridgerton Story","Supernatural", "Baby reindeer", "The Last Kingdom", "Beef", "Hightown", "The Simpsons", "Vikings Valhalla", "Blacklist" ]
     },
 
-]
+];
