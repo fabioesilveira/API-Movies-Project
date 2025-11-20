e77508d92d903a117fd5630fceeb62143ded55d5
Fabio Silveira <fabio.eds@hotmail.com>
2025-11-19 16:24:24 -0800

    updatedSpinner

--- pages/movies/main.js
+++ pages/movies/main.js
@@ -18,11 +18,26 @@ const btnOut = document.getElementById("btn-out");
 const divRetrieveUser = document.getElementById("retrieve-user-email");
 const divRetrieveName = document.getElementById("retrieve-user-name");
 
+function showLoader() {
+  const loader = document.getElementById("loader");
+  if (loader) loader.classList.remove("hide");
+}
+
+function hideLoader() {
+  const loader = document.getElementById("loader");
+  if (loader) loader.classList.add("hide");
+}
+
 async function createCard(movies) {
+  showLoader();   // <--- show spinner
+
   const data = await fetchAPI(movies);
+
+  hideLoader();   // <--- hide spinner
+
   divCards.innerHTML = "";
 
-  data.forEach((element) => {
+  data.forEach(element => {
     divCards.innerHTML += `
       <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
         <div class="card mb-3" style="width: 22rem;">
@@ -30,23 +45,16 @@ async function createCard(movies) {
           <div class="card-body">
             <button class="btn">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                   fill="currentColor"
-                   class="bi bi-star-fill icon-favorite icons-star"
-                   viewBox="0 0 16 16">
-                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 
-                         6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 
-                         0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 
-                         3.356.83 4.73c.078.443-.36.79-.746.592L8 
-                         13.187l-4.389 2.256z"/>
+                fill="currentColor" class="bi bi-star-fill icon-favorite icons-star">
+                <path d="M3.612 15.443c-.386..."/>
               </svg>
             </button>
             <h5 class="card-title">Title: ${element.Title}</h5>
             <p class="card-text">Description: ${element.Plot}</p>
             <p class="card-text">Released date: ${element.Released}</p>
-          </div> 
-        </div> 
-      </div>
-    `;
+          </div>
+        </div>
+      </div>`;
   });
 }
 
